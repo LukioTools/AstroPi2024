0e1aceb0c0a2387beb59f5b5b0e9e2dcebacf2fd
hiha <vili.kervinen.06@gmail.com>
2024-01-12 20:39:21 +0200

    quick

---
+++ main.py
@@ -0,0 +1,28 @@
+'''
+
+Rules:
+    No usage of threading (if not made sure they will die at end of the 10 minute, only using threading library)
+    Well documented
+    No errors
+    does not start system processes.
+    No networking
+    max runtime is 10min (we have to create a timer to kill itself)
+    have to use atleast one Sense HAT sensor.
+    No fucking or swearing
+
+python 3.9.2
+
+install libraires:
+- skyfield
+- picamera
+- gpiozero
+- nympy
+- logzero
+- opencv-python
+- sense_hat
+
+using pip3
+
+'''
+
+print("hello world")
